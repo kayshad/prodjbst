52472934f81960970fad20679b8c2f6edf8b0f1f
kayshad <kaisslama@gmail.com>
2020-06-13 17:26:58 +0200

    Add existing file

---
+++ monsite/k.py
@@ -0,0 +1,8 @@
+import os
+# import sys
+
+#print('le repertoire de base   '+os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+print('le repertoire de base   ' + os.path.dirname(os.path.abspath(__file__)))
+print(__file__)
